a552d7a7aae7bfcb6e6609e9e0f436740775368c
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-05-08 10:19:34 +0700

    Add files via upload

---
+++ BT1.py
@@ -0,0 +1,12 @@
+
+def thapHaNoi(n, toaMot, toaHai, toaBa):
+
+    if n == 1:
+        print("Chuyen tu", toaMot, "sang", toaBa)
+    else:
+        thapHaNoi(n-1,toaMot,toaBa ,toaHai)
+        print("Chuyen tu", toaMot, "sang", toaBa)
+        thapHaNoi(n-1,toaHai,toaMot, toaBa)
+       
+    
+
